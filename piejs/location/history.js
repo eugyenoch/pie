3bfdde4582ba4c810b5070fc2a0733c663a9804f
David U. Enoch <38564978+eugyenoch@users.noreply.github.com>
2020-04-13 00:06:54 +0100

    Create history.js

---
+++ piejs/location/history.js
@@ -0,0 +1,28 @@
+/*
+;The history object contains URLs visited by a user
+;a user can choose to go back or forward
+;to previosly loaded elements or URLs
+*/
+
+
+var hist={};
+
+//To go back one step in memory history
+hist.goBack=function(){
+window.history.back();
+};
+
+//To go back double steps
+hist.goBackDouble=function(){
+	window.history.back(-2);
+};
+
+//To go forward one step in memory history
+hist.goForward=function(){
+window.history.forward();
+};
+
+//To go forward double steps
+hist.goForwardDouble=function(){
+	window.history.forward(2);
+};
