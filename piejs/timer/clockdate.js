4193b883cf9edd96ef0f5c71bc5f10d9abb0f651
David U. Enoch <38564978+eugyenoch@users.noreply.github.com>
2020-04-16 15:28:45 +0100

    Update and rename piejs/timer.js to piejs/timer/clockdate.js

---
+++ piejs/timer/clockdate.js
@@ -0,0 +1,36 @@
+/*;
+* PieJS v1.1.0 ()
+* clockdate.js for PieJS timers
+* Copyright 2020 by Eugy Enoch 
+* https://eugyenoch.github.io/pie
+* For the Pie framework and library
+* Released under the MIT License
+*
+*See the readme file associated with this software for licencing and contact.
+*/
+"use strict";
+
+//Display digital clock using system time
+var clock={};
+
+clock.digitClock = setInterval(myTimer ,1000);
+function myTimer() {
+  var d = new Date();
+  document.getElementById("digitalClock").innerHTML = d.toLocaleTimeString();
+};
+
+//Stop the time - use the function on an HTML element
+clock.stopClock=function(){
+clearInterval(clock.digitClock)
+};
+
+//Display full date
+clock.dispDate=function(){
+var d = new Date();
+var e = d.getDate();
+var y = d.getFullYear();
+var months = 
+["January","February","March","April","May","June","July","August","September","October","November","December"];
+document.getElementById("fullDate").innerHTML = 
+e+" "+ months[d.getMonth()] + " " + y ;
+}
