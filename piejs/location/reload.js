b1a3dcc68b223cbc72d3152cc6106b9e2aab832c
David U. Enoch <38564978+eugyenoch@users.noreply.github.com>
2020-04-16 15:27:22 +0100

    Update reload.js

--- piejs/location/reload.js
+++ piejs/location/reload.js
@@ -1,8 +1,20 @@
+/*;
+* PieJS v1.1.0 ()
+* Reload.js for PieJS location model
+* Copyright 2020 by Eugy Enoch 
+* https://eugyenoch.github.io/pie/
+* For The Pie frameworks and library; 
+* Released under the MIT License.
+*
+*See the readme file associated with this software for licencing and contact.
+*/
+
 /*GENERAL INSTRUCTIONS
 ;Use the code in a function to reload document 
 ;using an javascript event
 ;that calls the function defined below.
 */
+"use strict";
 
 var reloads={}; 
 //This function will force a server reload of web document
